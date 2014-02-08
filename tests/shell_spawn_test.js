072f88429a62cc2f5a41a81ae9471ea5329125fd
Sergey Krilov <serg.kr@gmail.com>
2014-02-07 21:20:59 -0500

    Refactor shared logic in tests
    Extract the part that tests error/stderr as this is going to be a common pattern for ensuing tests

--- tests/shell_spawn_test.js
+++ tests/shell_spawn_test.js
@@ -4,13 +4,17 @@ var grunt = require('grunt');
 var fs = require('fs');
 var exec = require('child_process').exec;
 
+var shouldNotError = function (test, error, stderr) {
+  test.equal(error, null, 'Error: ' + error);
+  test.equal(stderr, '', 'Error: ' + stderr);
+};
+
 exports['grunt-shell-spawn'] = {
   defaultSync: function(test) {
     test.expect(2);
 
     exec('./node_modules/.bin/grunt shell:defaultSync', function(error, stdout, stderr) {
-      test.equal(error, null, 'It should not error');
-      test.equal(stderr, '', 'It should not error');
+      shouldNotError(test, error, stderr);
       test.done();
     });
   },
@@ -19,8 +23,7 @@ exports['grunt-shell-spawn'] = {
     test.expect(2);
 
     exec('./node_modules/.bin/grunt repeat', function(error, stdout, stderr) {
-      test.equal(error, null, 'It should not error');
-      test.equal(stderr, '', 'It should not error');
+      shouldNotError(test, error, stderr);
       test.done();
     });
   }
